6b7ec2b6db1698db27bc64737516c0bd07ee7217
Student03 <phrombutr@gmail.comm>
2017-06-14 10:31:17 +0700

    Check User OK

--- Esay Autthen/ViewController.swift
+++ Esay Autthen/ViewController.swift
@@ -9,24 +9,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Implicit
+    var strUser: String?
+    var strPassWord: String?
     
-    @IBAction func loginButton(_ sender: Any) {
-    }
-    @IBOutlet weak var passWordTextField: UITextField!
-       
+    
+    
+    //สร้าง Object View
+    
     @IBOutlet weak var userTextField: UITextField!
     
+    @IBOutlet weak var passWordTextField: UITextField!
+    
     @IBOutlet weak var messageLable: UILabel!
+    
+    
+    //สร้าง Method for Button Action
+    
+    @IBAction func loginButton(_ sender: Any) {
+        
+        //Get Value From TextField
+        strUser = userTextField.text
+        strPassWord = passWordTextField.text
+        
+        //Show User & Pass on Console
+        print("User ==> \(strUser!)")
+        print("PassWord ==> \(strPassWord!)")
+    
+        
+        //Find Count String
+        let intUser = strUser?.characters.count
+        let intPassWord = strPassWord?.characters.count
+        print("intUser ==> \(intUser!)")
+        print("intPassWord ==> \(intPassWord!)")
+        
+        //Call checkSpace
+        if checkSpace(myString: strUser!) {
+            print("User OK")
+        }   else {
+            print("Blank User")
+        }
+        
+    
+    }   //Login Button
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }   //Main Method
+    
+    func checkSpace(myString: String) -> Bool {
+        let intString = myString.characters.count
+        var result: Bool = true
+        
+        if intString == 0 {
+            //Have Space
+            result = false
+        }
+        return result
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }   //End didReceive Method
 
 
 }
