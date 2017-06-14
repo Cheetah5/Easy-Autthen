75aa8fae4fbca226b8114b90b31f3db1144a0c36
Student03 <phrombutr@gmail.comm>
2017-06-14 09:30:30 +0700

    Initial View Success

--- Esay Autthen/ViewController.swift
+++ Esay Autthen/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    @IBOutlet weak var passWordTextField: UITextField!
+       
+    @IBOutlet weak var userTextField: UITextField!
+    
+    @IBOutlet weak var messageLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
